7321310d61a5db6ece9aba148b1838ea331c8734
Pravesh Khatana <pravesh.khatana@gmail.com>
2018-10-07 09:27:07 +0530

    init

---
+++ src/iot.android.ts
@@ -0,0 +1,186 @@
+import { Observable } from "tns-core-modules/data/observable/observable";
+import * as app from "tns-core-modules/application/application";
+import * as utils from "tns-core-modules/utils/utils";
+
+declare var com;
+
+declare var IotService;
+interface IotService {}
+
+export class Iot extends Observable {
+  private keystoreName: string = "plugin_iot";
+  private keystorePassword: string = "iot_pass";
+  private certificateId: string = "IOT";
+  private clientId: string;
+  private credentials;
+  private callback: any;
+
+  private iotService: IotService;
+
+  //
+  private CUSTOMER_SPECIFIC_ENDPOINT: string;
+
+  private COGNITO_POOL_ID: string;
+
+  private AWS_IOT_POLICY_NAME: string;
+
+  // Region of AWS IoT
+  private MY_REGION: any;
+
+  private KEYSTORE_NAME: string;
+  // Password for the private key in the KeyStore
+  private KEYSTORE_PASSWORD: string;
+  // Certificate and key aliases in the KeyStore
+  private CERTIFICATE_ID: string;
+
+  private ctx;
+  public mIotAndroidClient: any;
+  private mqttManager;
+
+  private identityId: string;
+  private keystorePath: string;
+
+  private clientKeyStore: any = null;
+
+  private credentialsProvider: any;
+  private cred: any;
+
+  constructor(
+    endpoint,
+    cognitoPoolId,
+    policyName,
+    clientId,
+    credentials,
+    callback
+  ) {
+    super();
+    this.CUSTOMER_SPECIFIC_ENDPOINT = endpoint;
+    this.COGNITO_POOL_ID = cognitoPoolId;
+    this.AWS_IOT_POLICY_NAME = policyName;
+    this.clientId = clientId;
+    this.credentials = credentials;
+    this.MY_REGION = com.amazonaws.regions.Regions.US_WEST_2;
+    this.KEYSTORE_NAME = "iot_keystore3";
+    this.KEYSTORE_PASSWORD = "password3";
+    this.CERTIFICATE_ID = "default3";
+    this.callback = callback;
+
+    this.ctx = utils.ad.getApplicationContext();
+    //console.log("IOT ANdroid");
+    this.iotService = new com.tns.IotService(
+      this.ctx,
+      endpoint,
+      cognitoPoolId,
+      policyName,
+      clientId,
+      credentials.identityId,
+      credentials.accessKeyId,
+      credentials.secretAccessKey,
+      credentials.sessionToken
+    );
+    this.attachPolicy();
+  }
+  public connect() {
+    try {
+      let mqttCallback = new com.amazonaws.mobileconnectors.iot.AWSIotMqttClientStatusCallback(
+        (status, throwable) => {
+          //console.log(status);
+        }
+      );
+
+      mqttCallback.onStatusChanged = (status, throwable) => {
+        //console.log(status);
+        this.callback &&
+          this.callback.onMqttEventCallback &&
+          this.callback.onMqttEventCallback(status);
+      };
+
+      this.mqttManager.connect(
+        this.credentialsProvider,
+        mqttCallback
+      );
+    } catch (e) {
+      console.log("Disconnect error.", e);
+    }
+  }
+
+  //
+  private attachPolicy() {
+    this.cred = new com.amazonaws.auth.BasicSessionCredentials(
+      this.credentials.accessKeyId,
+      this.credentials.secretAccessKey,
+      this.credentials.sessionToken
+    );
+    this.credentialsProvider = new com.amazonaws.auth.AWSStaticCredentialsProvider(
+      this.cred
+    );
+    let region = com.amazonaws.regions.Region.getRegion(this.MY_REGION);
+
+    // MQTT Client AWSIotMqttManager
+    this.mqttManager = new com.amazonaws.mobileconnectors.iot.AWSIotMqttManager(
+      this.clientId,
+      this.CUSTOMER_SPECIFIC_ENDPOINT
+    );
+
+    // Set keepalive to 10 seconds.  Will recognize disconnects more quickly but will also send
+    // MQTT pings every 10 seconds.
+    this.mqttManager.setKeepAlive(10);
+
+    // Set Last Will and Testament for MQTT.  On an unclean disconnect (loss of connection)
+    // AWS IoT will publish this message to alert other clients.
+    let lwt = new com.amazonaws.mobileconnectors.iot.AWSIotMqttLastWillAndTestament(
+      "my/lwt/topic",
+      "Android client lost connection",
+      com.amazonaws.mobileconnectors.iot.AWSIotMqttQos.QOS0
+    );
+    this.mqttManager.setMqttLastWillAndTestament(lwt);
+  }
+
+  public disconnect() {
+    try {
+      this.mqttManager.disconnect();
+      console.log("Disconnected");
+    } catch (e) {
+      console.log("Disconnect error.", e);
+    }
+  }
+
+  public publish(msg, topic) {
+    try {
+      this.mqttManager.publishString(
+        msg,
+        topic,
+        com.amazonaws.mobileconnectors.iot.AWSIotMqttQos.QOS0
+      );
+    } catch (e) {
+      console.log("Publish error.", e);
+    }
+  }
+
+  public subscribe(topic) {
+    try {
+      let mqttCallback = new com.amazonaws.mobileconnectors.iot.AWSIotMqttNewMessageCallback(
+        (topic, data) => {
+          console.log("TOPIC: " + topic);
+        }
+      );
+
+      mqttCallback.onMessageArrived = (topic, data) => {
+        console.log("Message arrived:");
+        console.log("   Topic: " + topic);
+        console.log(" Message: " + data.toString());
+        this.callback &&
+          this.callback.onSubscribe &&
+          this.callback.onSubscribe(topic, data);
+      };
+
+      this.mqttManager.subscribeToTopic(
+        topic,
+        com.amazonaws.mobileconnectors.iot.AWSIotMqttQos.QOS0,
+        mqttCallback
+      );
+    } catch (e) {
+      console.log("Subscription error.", e);
+    }
+  }
+}
---
+++ src/iot.ios.ts
@@ -0,0 +1,290 @@
+import { Observable } from "tns-core-modules/data/observable";
+
+declare var AWSIoT,
+  AWSIoTDataManager,
+  AWSIoTManager,
+  AWSCognitoCredentialsProvider,
+  AWSEndpoint,
+  AWSServiceConfiguration,
+  AWSServiceManager,
+  AWSBasicSessionCredentialsProvider,
+  AWSIoTAttachPrincipalPolicyRequest;
+declare var AWSRegionType;
+interface AWSRegionType {}
+
+export class Iot extends Observable {
+  private IOT_ENDPOINT: string;
+  private ASWIoTDataManager = "MyIotDataManager";
+  private CognitoIdentityPoolId: string;
+  private PolicyName: string;
+  private AWSRegion: AWSRegionType = AWSRegionType.USWest2;
+  private clientId: string;
+  private identityId: string;
+  private credentials;
+  private callback: any;
+
+  private CertificateSigningRequestCommonName = "IOT plugin Application";
+  private CertificateSigningRequestCountryName = "USA";
+  private CertificateSigningRequestOrganizationName = "IOT";
+  private CertificateSigningRequestOrganizationalUnitName = "Software";
+
+  //
+  private connected: boolean = false;
+  private iotDataManager: AWSIoTDataManager;
+  private iotManager: AWSIoTManager;
+  private iot: AWSIoT;
+
+  constructor(
+    endpoint,
+    cognitoPoolId,
+    policyName,
+    clientId,
+    credentials,
+    callback
+  ) {
+    super();
+    this.AWSRegion = AWSRegionType.USWest2;
+    this.IOT_ENDPOINT = endpoint;
+    this.CognitoIdentityPoolId = cognitoPoolId;
+    this.PolicyName = policyName;
+    this.clientId = clientId;
+    this.identityId = credentials.identityId;
+    this.credentials = credentials;
+    this.callback = callback;
+
+    this.init();
+    console.log(this.credentials.sessionToken);
+    console.log(this.identityId);
+  }
+
+  public disconnect() {
+    this.iotDataManager.disconnect();
+  }
+  public connect() {
+    //console.log("connect");
+    if (this.credentials != "" && this.credentials != null) {
+      this.authConnect();
+    } else {
+      this.attachPolicy();
+    }
+  }
+  public publish(message: string, topic: string) {
+    let qos: AWSIoTMQTTQoS = AWSIoTMQTTQoS.MessageDeliveryAttemptedAtLeastOnce;
+    this.iotDataManager.publishStringOnTopicQoS(message, topic, qos);
+  }
+  public subscribe(topic: string) {
+    this.iotDataManager.subscribeToTopicQoSExtendedCallback(
+      topic,
+      AWSIoTMQTTQoS.MessageDeliveryAttemptedAtLeastOnce,
+      (payload, p2, p3) => {
+        let stringVal = NSString.alloc().initWithDataEncoding(
+          p3,
+          NSUTF8StringEncoding
+        );
+        //console.log("received: "+stringVal);
+        this.callback &&
+          this.callback.onSubscribe &&
+          this.callback.onSubscribe(topic, stringVal);
+      }
+    );
+  }
+  private init() {
+    let credentialsProvider;
+    if (this.credentials != "" && this.credentials != null) {
+      credentialsProvider = new AWSBasicSessionCredentialsProvider(
+        this.credentials.accessKeyId,
+        this.credentials.secretAccessKey,
+        this.credentials.sessionToken
+      );
+    } else {
+      credentialsProvider = new AWSCognitoCredentialsProvider(
+        this.AWSRegion,
+        this.CognitoIdentityPoolId
+      );
+    }
+
+    let iotEndPoint = new AWSEndpoint(this.IOT_ENDPOINT);
+
+    // // Configuration for AWSIoT control plane APIs
+    let iotConfiguration = new AWSServiceConfiguration(
+      this.AWSRegion,
+      credentialsProvider
+    );
+
+    // Configuration for AWSIoT data plane APIs
+    let iotDataConfiguration = new AWSServiceConfiguration(
+      this.AWSRegion,
+      iotEndPoint,
+      credentialsProvider
+    );
+
+    AWSServiceManager.defaultServiceManager().defaultServiceConfiguration = iotConfiguration;
+
+    this.iotManager = AWSIoTManager.defaultIoTManager();
+    this.iot = AWSIoT.defaultIoT();
+
+    AWSIoTDataManager.registerIoTDataManagerWithConfigurationForKey(
+      iotDataConfiguration,
+      this.ASWIoTDataManager
+    );
+
+    this.iotDataManager = AWSIoTDataManager.IoTDataManagerForKey(
+      this.ASWIoTDataManager
+    );
+  }
+
+  private authConnect() {
+    let attachPrincipalPolicyRequest = new AWSIoTAttachPrincipalPolicyRequest();
+
+    attachPrincipalPolicyRequest.policyName = this.PolicyName;
+    attachPrincipalPolicyRequest.principal = this.identityId;
+
+    // Attach the policy to the certificate
+    this.iot.attachPrincipalPolicyCompletionHandler(
+      attachPrincipalPolicyRequest,
+      e => {
+        console.log(e);
+      }
+    );
+    this.iotDataManager.connectUsingWebSocketWithClientIdCleanSessionStatusCallback(
+      this.clientId,
+      true,
+      status => {
+        this.callback &&
+          this.callback.onMqttEventCallback &&
+          this.callback.onMqttEventCallback(status);
+      }
+    );
+  }
+
+  //Attach Policy If user is Guest/unAuth
+  private attachPolicy() {
+    let defaults = NSUserDefaults.standardUserDefaults;
+    let certificateId = defaults.stringForKey("certificateId");
+    console.log(certificateId);
+    if (certificateId === null) {
+      console.log("No identity available, searching bundle...");
+      // No certificate ID has been stored in the user defaults; check to see if any .p12 files
+      // exist in the bundle.
+      let myBundle = NSBundle.mainBundle;
+      let myImages = myBundle.pathsForResourcesOfTypeInDirectory("p12", ""); //.paths(forResourcesOfType: "p12" as String, inDirectory:nil)
+
+      //
+      if (myImages.count > 0) {
+        // At least one PKCS12 file exists in the bundle.  Attempt to load the first one
+        // into the keychain (the others are ignored), and set the certificate ID in the
+        // user defaults as the filename.  If the PKCS12 file requires a passphrase,
+        // you'll need to provide that here; this code is written to expect that the
+        // PKCS12 file will not have a passphrase.
+        let data = NSData.dataWithContentsOfFile(myImages[0]);
+        if (data) {
+          console.log("found identity " + myImages[0] + " importing...");
+
+          if (
+            AWSIoTManager.importIdentityFromPKCS12DataPassPhraseCertificateId(
+              data,
+              "",
+              myImages[0]
+            )
+          ) {
+            // Set the certificate ID and ARN values to indicate that we have imported
+            // our identity from the PKCS12 file in the bundle.
+            defaults.setValueForKey(myImages[0], "certificateId");
+            defaults.setValueForKey("from-bundle", "certificateArn");
+
+            console.log("Using certificate: " + myImages[0]);
+
+            this.iotDataManager.connectWithClientIdCleanSessionCertificateIdStatusCallback(
+              this.clientId,
+              true,
+              myImages[0],
+              status => {
+                this.callback &&
+                  this.callback.onMqttEventCallback &&
+                  this.callback.onMqttEventCallback(status);
+              }
+            );
+          }
+        }
+      }
+      //
+
+      //
+      certificateId = defaults.stringForKey("certificateId");
+      if (certificateId === null) {
+        console.log("No identity found in bundle, creating one...");
+
+        let csrDictionary = new (NSDictionary as any)(
+          [
+            this.CertificateSigningRequestCommonName,
+            this.CertificateSigningRequestCountryName,
+            this.CertificateSigningRequestOrganizationName,
+            this.CertificateSigningRequestOrganizationalUnitName
+          ],
+          [
+            "commonName",
+            "countryName",
+            "organizationName",
+            "organizationalUnitName"
+          ]
+        );
+
+        this.iotManager.createKeysAndCertificateFromCsrCallback(
+          csrDictionary,
+          response => {
+            if (response) {
+              defaults.setValueForKey(response.certificateId, "certificateId");
+              defaults.setValueForKey(
+                response.certificateArn,
+                "certificateArn"
+              );
+              certificateId = response.certificateId;
+              console.log(
+                "response certificateArn: " + response.certificateArn
+              );
+              console.log("response certificateId: " + response.certificateId);
+
+              let attachPrincipalPolicyRequest = new AWSIoTAttachPrincipalPolicyRequest();
+
+              attachPrincipalPolicyRequest.policyName = this.PolicyName;
+              attachPrincipalPolicyRequest.principal = response.certificateArn;
+
+              // Attach the policy to the certificate
+              this.iot.attachPrincipalPolicyCompletionHandler(
+                attachPrincipalPolicyRequest,
+                e => {
+                  console.log(e);
+                }
+              );
+              this.iotDataManager.connectWithClientIdCleanSessionCertificateIdStatusCallback(
+                this.clientId,
+                true,
+                certificateId,
+                status => {
+                  this.callback &&
+                    this.callback.onMqttEventCallback &&
+                    this.callback.onMqttEventCallback(status);
+                }
+              );
+            } else {
+              console.log("noooo");
+            }
+          }
+        );
+      }
+      //
+    } else {
+      // Connect to the AWS IoT service
+      this.iotDataManager.connectWithClientIdCleanSessionCertificateIdStatusCallback(
+        this.clientId,
+        true,
+        certificateId,
+        status => {
+          this.callback &&
+            this.callback.onMqttEventCallback &&
+            this.callback.onMqttEventCallback(status);
+        }
+      );
+    }
+  }
+}
